134e1263909303cee20a6ecaeb6e527ec7dc4e13
jws1218 <wsjung1218@gmail.com>
2025-08-22 09:07:06 +0900

    token 갱신
    token 갱신

--- constants/server.ts
+++ constants/server.ts
@@ -2,4 +2,8 @@
 export const SERVER_URL = "https://mohaemoyong.store";
 
 // token
+<<<<<<< Updated upstream
 export const TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiaWF0IjoxNzU1Nzg5MDk1LCJleHAiOjE3NTU4MTA2OTV9.GaQW8xTcMeZ8S4npfCXX_67XPWpuEYwPXiqVdosilY5pizaw7sW3yXKBYWIfX4iMy7JFsna9vcQsoYJvJo-M3A";
+=======
+export const TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyIiwiaWF0IjoxNzU1ODIxMDI5LCJleHAiOjE3NTU4NDI2Mjl9._BKfmc10ZXua4Ettb8TqYnSU1SLTLiicqBQXKgZlH_3RR2APPBWZcWp8Nq2WlIMHG-vxkKWDxao3kCuU_1VDsg";
+>>>>>>> Stashed changes
