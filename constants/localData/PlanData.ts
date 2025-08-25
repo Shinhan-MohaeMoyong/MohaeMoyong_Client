a75f4367f0915d9d0d8e53e6f3d53128ec9b0214
jws1218 <wsjung1218@gmail.com>
2025-08-25 22:23:05 +0900

    뭐해영 헤더 기능 구현
    구현했습니다. 현재 주에 대한 일정도 따로 나옴. 근데 갑자기 백엔드 통신이 ㅏㄴ되서 이건 해결하고 다시 커밋할 예정

--- constants/localData/PlanData.ts
+++ constants/localData/PlanData.ts
@@ -6,16 +6,16 @@ export const samplePlans: PlanEntity[] = [
         planId: 1,
         title: "컴퓨터네트워크",
         place: "55호관",
-        startTime: "2024-01-15T11:00:00",
-        endTime: "2024-01-15T12:30:00",
+        startTime: "2025-08-25T11:00:00",
+        endTime: "2025-08-25T12:30:00",
         new: false
     },
     {
         planId: 2,
         title: "스터디 모임",
         place: "동아리실",
-        startTime: "2024-08-22T13:00:00",
-        endTime: "2024-08-22T16:00:00",
+        startTime: "2025-08-22T13:00:00",
+        endTime: "2025-08-22T16:00:00",
         new: true
     },
     {
