f35ea996750ad82d2762dc0f79a51c9e1e4934e0
sangji <wsjung1218@gmail.com>
2025-08-26 09:56:47 +0900

    탭바 모아영 누르면 화면
    ㅇ

--- app/(tabs)/Moayoung.tsx
+++ app/(tabs)/Moayoung.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import ScheduleCalendarScreen from '@/screens/ScheduleCalendarScreen';
+import { StyleSheet, View } from 'react-native';
 
 export default function ExploreScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>탐색</Text>
-      <Text style={styles.subtitle}>이곳에서 새로운 친구들을 찾아보세요!</Text>
+      <ScheduleCalendarScreen />
     </View>
   );
 }
